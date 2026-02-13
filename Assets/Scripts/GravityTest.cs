c6d6abff58539b779bc013ccd8a8d4896c9c7dce
Sunny <the.sunny.cheng@gmail.com>
2026-02-13 16:02:02 -0500

    Space prototype done

---
+++ Assets/Scripts/GravityTest.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject Planet;
+    public GameObject PlayerPlaceholder;
+
+
+    public float speed = 4;
+    
+
+    float gravity = 100;
+    bool OnGround = false;
+
+
+    float distanceToGround;
+    Vector3 Groundnormal;
+
+
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        //GroundControl
+
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 10))
+        {
+
+            distanceToGround = hit.distance;
+            Groundnormal = hit.normal;
+
+            if (distanceToGround <= 0.2f)
+            {
+                OnGround = true;
+            }
+            else
+            {
+                OnGround = false;
+            }
+
+
+        }
+
+
+        //GRAVITY and ROTATION
+
+        Vector3 gravDirection = (transform.position - Planet.transform.position).normalized;
+
+        if (OnGround == false)
+        {
+            rb.AddForce(gravDirection * -gravity);
+
+        }
+
+        //
+
+        Quaternion toRotation = Quaternion.FromToRotation(transform.up, Groundnormal) * transform.rotation;
+        transform.rotation = toRotation;
+
+
+
+    }
+}
