3f03a62dd66404c31b0f1ecac09df618f8dcd119
kstrong03 <kat.strong2003@gmail.com>
2026-02-26 08:34:07 -0500

    dance scene works!

---
+++ Assets/Scripts/PotionOnCollision.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PotionOnCollision : MonoBehaviour
+{
+    private void DestroyOnCollision(Collision2D collision){
+
+        if (collision.gameObject.CompareTag("Red") || collision.gameObject.CompareTag("Yellow") || collision.gameObject.CompareTag("Blue"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void ColorMixer(){
+
+
+
+    }
+}
